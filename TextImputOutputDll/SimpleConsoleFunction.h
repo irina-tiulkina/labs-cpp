16b6dc86307bb0f90735859667ba173f7da2d38c
Irina Tiulkina <irina.tyulkina10@mail.ru>
2022-09-07 21:55:52 +0300

    [add] работа с консолью и std::string

---
+++ TextImputOutputDll/SimpleConsoleFunction.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <string>
+#include <iostream>
+class SimpleConsoleFunction
+{
+public:
+  static void StringToConsole(std::string text);
+  static std::string StringFromConsoleByWord();
+  static std::string StringFromConsoleByLine();
+};
+
