2e8657d52fb174567663f42dce7c7dfb6c645b49
Irina Tiulkina <irina.tyulkina10@mail.ru>
2022-09-11 17:49:51 +0300

    [add] вторая лабораторная

--- Labs/Source.cpp
+++ Labs/Source.cpp
@@ -3,9 +3,11 @@
 #include "../UserControl/L1.h"
 #include "../UserControl/UserInterfaceClaim.h"
 #include "../ConsoleWorkLib/SimpleConsoleFunction.cpp"
+#include <L2.h>
 
 const std::string exitNum = "0";
 const std::string labOneNum = "1";
+const std::string labTwoNum = "2";
 
 int main() {
   SetConsoleCP(1251);
@@ -15,11 +17,13 @@ int main() {
 
   while (true) {
     L1 lab1;
+    L2 lab2;
 
     std::string menu = "Меню: \n";
     std::string exitMenu = exitNum + ". Выйти из программы\n";
     std::string labMenuOne = labOneNum + ". " + lab1.GetTitleTask() + "\n";
-    scf_t::StringToConsole(menu + exitMenu + labMenuOne);
+    std::string labMenuTwo = labTwoNum + ". " + lab2.GetTitleTask() + "\n";
+    scf_t::StringToConsole(menu + exitMenu + labMenuOne + labMenuTwo);
 
     scf_t::StringToConsole("Выберете пункт меню:");
     std::string selectNum = scf_t::StringFromConsoleByLine();
@@ -31,6 +35,12 @@ int main() {
     if (selectNum == labOneNum) {
       lab1.AboutProgrammToConsole();
       lab1.StartCycleProgramm();
+      continue;
+    }
+
+    if (selectNum == labTwoNum) {
+      lab2.AboutProgrammToConsole();
+      lab2.StartCycleProgramm();
       continue;
     }
 
---
+++ UserControl/L2.cpp
@@ -0,0 +1,2 @@
+#include "pch.h"
+#include "L2.h"
